fe6e2d7798ab1d3763aaa0b735a2a718e59d4cea
Jose Manuel <92829044+jbohorper@users.noreply.github.com>
2022-10-12 20:51:57 -0500

    Delete build

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-require("./database");
-var _config = require("./config.js");
-require("./libs/initialSetup.js");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_app["default"].listen(_config.PORT);
-console.log("Server on port", _app["default"].get("port"));
